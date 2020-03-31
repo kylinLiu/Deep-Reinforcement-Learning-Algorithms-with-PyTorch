44030d30342bfc1c06503b3d37719f17656b10e1
Liumumu <247651305@qq.com>
2020-03-31 10:58:44 +0800

    Update SAC_Discrete.py

--- agents/actor_critic_agents/SAC_Discrete.py
+++ agents/actor_critic_agents/SAC_Discrete.py
@@ -51,7 +51,7 @@ class SAC_Discrete(SAC):
     def produce_action_and_action_info(self, state):
         """Given the state, produces an action, the probability of the action, the log probability of the action, and
         the argmax action"""
-#         print("state",state)
+        print("state",state)
         action_probabilities = self.actor_local(state)
         print("action_probabilities",action_probabilities)
         max_probability_action = torch.argmax(action_probabilities).unsqueeze(0)
